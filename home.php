2384a941467d7d5622875d61f298d6d947ea9156
Jotah <ojotahc@gmail.com>
2026-05-11 20:26:38 -0300

    adicionando login e sessões

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AppPet</title>
+    <link rel="stylesheet" href="css/formulario.css">
+</head>
+<body>
+    <h1>Seja bem-vindo(a)</h1>
+    <div class="box">
+        <a href="login.php">Login</a>
+        <a href="fomulario.php">Cadastre-se</a>
+    </div>
+</body>
+</html>
